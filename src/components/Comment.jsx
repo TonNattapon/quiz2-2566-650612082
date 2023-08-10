69d3de943ce6d415589e9135cb689fbf1cc17325
= <=>
2023-08-10 15:09:48 +0700

    Commit and push #1

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { Reply } from "./Reply";
+
 export const Comment = ({
   userImagePath,
   username,
@@ -5,10 +8,47 @@ export const Comment = ({
   likeNum,
   replies,
 }) => {
+  const [isLike, setIsLike] = useState(true);
+
+  function checkLike(props) {
+    props.likeNum = likeNumber;
+    if (likeNumber > 0) {
+      setIsLike(true);
+    } else {
+      setIsLike(false);
+    }
+  }
   return (
     <div>
-      your code here...
-      {/* map-loop render Reply component here */}
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{username}</span>
+          <br />
+          <span>{commentText}</span>
+          <div className="d-flex align-items-center gap-1">
+            {isLike ? <img src="/like.svg" width={20}></img> : null}
+            {isLike ? <span className="text-muted">{likeNum}</span> : null}
+          </div>
+        </div>
+      </div>
+      <Reply
+        key={replies.username}
+        replyText={replies.commentText}
+        userImagePath={replies.userImagePath}
+        username={replies.username}
+        likeNum={replies.likeNum}
+        replies={replies.replies}
+      />
     </div>
   );
 };
--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,20 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+import React from "react";
+
+export const PostOwner = ({ name, studentId }) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/Profile_pic.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5">
+          {name} {studentId}
+        </span>
+      </div>
+    </div>
+  );
 };
